081e833d9ce0bb743185df655f97742be316e0c5
smpn6blora <109122900+smpn6blora@users.noreply.github.com>
2022-11-07 04:48:40 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,51 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    sprite.say("oww!", 500)
+})
+sprites.onDestroyed(SpriteKind.Projectile, function (sprite) {
+    info.changeScoreBy(2)
+})
+let projectile: Sprite = null
+let mySprite = sprites.create(img`
+    . . . . . . 5 . 5 . . . . . . . 
+    . . . . . f 5 5 5 f f . . . . . 
+    . . . . f 1 5 2 5 1 6 f . . . . 
+    . . . f 1 6 6 6 6 6 1 6 f . . . 
+    . . . f 6 6 f f f f 6 1 f . . . 
+    . . . f 6 f f d d f f 6 f . . . 
+    . . f 6 f d f d d f d f 6 f . . 
+    . . f 6 f d 3 d d 3 d f 6 f . . 
+    . . f 6 6 f d d d d f 6 6 f . . 
+    . f 6 6 f 3 f f f f 3 f 6 6 f . 
+    . . f f 3 3 5 3 3 5 3 d f f . . 
+    . . . f d f 3 5 5 3 f f d f . . 
+    . . . f d f 3 3 3 3 3 f f . . . 
+    . . . f f 3 5 3 3 5 3 3 f . . . 
+    . . . . f f f f f f f f f . . . 
+    . . . . . . . . . f f . . . . . 
+    `, SpriteKind.Player)
+mySprite.setStayInScreen(true)
+controller.moveSprite(mySprite)
+info.setScore(0)
+info.setLife(3)
+game.onUpdateInterval(1000, function () {
+    projectile = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . b 5 b . . . 
+        . . . . . . . . . b 5 b . . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 5 5 5 5 5 b . . . 
+        . . . . b b 5 d 1 f 5 d 4 c . . 
+        . . . . b 5 5 1 f f d d 4 4 4 b 
+        . . . . b 5 5 d f b 4 4 4 4 b . 
+        . . . b d 5 5 5 5 4 4 4 4 b . . 
+        . b b d d d 5 5 5 5 5 5 5 b . . 
+        b d d d b b b 5 5 5 5 5 5 5 b . 
+        c d d b 5 5 d c 5 5 5 5 5 5 b . 
+        c b b d 5 d c d 5 5 5 5 5 5 b . 
+        c b 5 5 b c d d 5 5 5 5 5 5 b . 
+        b b c c c d d d 5 5 5 5 5 d b . 
+        . . . . c c d d d 5 5 5 b b . . 
+        . . . . . . c c c c c b b . . . 
+        `, 40 + info.score(), 0)
+    projectile.y = randint(0, 7) * 16
+})
